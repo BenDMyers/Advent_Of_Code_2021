d064c100d5e480e82fb355d7277cad8d4735751b
Ben Myers <ben.myers@okstate.edu>
2021-12-01 23:03:45 -0600

    Day 2 - Part 1

---
+++ 02/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const lines = fs
+	.readFileSync(`${__dirname}/.input`, 'utf-8')
+	.split('\n')
+	.map(line => {
+		const [direction, amountString] = line.split(' ');
+		const amount = Number(amountString);
+		return [direction, amount];
+	});
+
+let horizontalOffset = 0;
+let verticalOffset = 0;
+
+for (const step of lines) {
+	const [direction, amount] = step;
+	switch (direction) {
+		case 'forward':
+			horizontalOffset += amount;
+			break;
+		case 'down':
+			verticalOffset += amount;
+			break;
+		case 'up':
+			verticalOffset -= amount;
+			break;
+	}
+}
+
+console.log(horizontalOffset * verticalOffset);
