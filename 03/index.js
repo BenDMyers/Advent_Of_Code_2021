27d634385638aa3f1fd076606a38a54d3392f176
Ben Myers <ben.myers@okstate.edu>
2021-12-02 23:44:06 -0600

    Day 3 - Part 2

--- 03/index.js
+++ 03/index.js
@@ -4,54 +4,117 @@ const lines = fs
 	.split('\n')
 	.map(line => line.split(''));
 
-
-let gammaBits = '';
-let epsilonBits = '';
-
-for (let col = 0; col < lines[0].length; col++) {
-	let zeroes = 0;
-	let ones = 0;
-	for (let row = 0; row < lines.length; row++) {
-		let bit = lines[row][col];
-		if (bit === '0') {
-			zeroes++;
+// Part 1
+(function () {
+	let gammaBits = '';
+	let epsilonBits = '';
+	
+	for (let col = 0; col < lines[0].length; col++) {
+		let zeroes = 0;
+		let ones = 0;
+		for (let row = 0; row < lines.length; row++) {
+			let bit = lines[row][col];
+			if (bit === '0') {
+				zeroes++;
+			} else {
+				ones++;
+			}
+		}
+	
+		if (zeroes > ones) {
+			gammaBits += '0';
+			epsilonBits += '1';
 		} else {
-			ones++;
+			gammaBits += '1';
+			epsilonBits += '0';
+		}
+	}
+	
+	let gamma = Number.parseInt(gammaBits, 2);
+	let epsilon = Number.parseInt(epsilonBits, 2);
+	console.log(gamma * epsilon);
+})();
+
+// Part 2
+(function () {
+	/**
+	 * 
+	 * @param {string[]} bitstrings 
+	 * @param {number} index
+	 * @returns {string}
+	 */
+	function getMostCommonBit(bitstrings, index) {
+		let zeroes = 0;
+		let ones = 0;
+
+		for (const bitstring of bitstrings) {
+			if (bitstring.charAt(index) === '0') {
+				zeroes++;
+			} else {
+				ones++;
+			}
+		}
+
+		if (zeroes > ones) {
+			return '0';
+		} else {
+			return '1';
 		}
 	}
 
-	if (zeroes > ones) {
-		gammaBits += '0';
-		epsilonBits += '1';
-	} else {
-		gammaBits += '1';
-		epsilonBits += '0';
+	/**
+	 * 
+	 * @param {string[]} bitstrings 
+	 * @param {number} index
+	 * @returns {string}
+	 */
+	 function getLeastCommonBit(bitstrings, index) {
+		let zeroes = 0;
+		let ones = 0;
+
+		for (const bitstring of bitstrings) {
+			if (bitstring.charAt(index) === '0') {
+				zeroes++;
+			} else {
+				ones++;
+			}
+		}
+
+		if (zeroes <= ones) {
+			return '0';
+		} else {
+			return '1';
+		}
 	}
-}
 
-let gamma = Number.parseInt(gammaBits, 2);
-let epsilon = Number.parseInt(epsilonBits, 2);
-console.log(gamma * epsilon);
+	let oxygenGeneratorCandidates = lines.map(line => line.join(''));
+	let oxygenGeneratorBitIndex = 0;
+	while (oxygenGeneratorCandidates.length > 1) {
+		let mostCommonBit = getMostCommonBit(oxygenGeneratorCandidates, oxygenGeneratorBitIndex);
+		if (mostCommonBit === '01') {
+			mostCommonBit = '1';
+		}
 
-let oxygenRatingCandidates = [...lines];
-let oxygenRatingBitPosition = 0;
-while (oxygenRatingCandidates.length > 1) {
-	oxygenRatingCandidates = oxygenRatingCandidates
-		.filter(candidate => candidate[oxygenRatingBitPosition] === gammaBits.charAt(oxygenRatingBitPosition));
+		oxygenGeneratorCandidates = oxygenGeneratorCandidates.filter(candidate => candidate.charAt(oxygenGeneratorBitIndex) === mostCommonBit);
+		oxygenGeneratorBitIndex++;
+	}
 	
-	console.log(oxygenRatingCandidates)
-	oxygenRatingBitPosition++;
-}
+	const [oxygenGeneratorRatingBitstring] = oxygenGeneratorCandidates;
+	const oxygenGeneratorRating = parseInt(oxygenGeneratorRatingBitstring, 2);
 
-let co2RatingCandidates = [...lines];
-let co2RatingBitPosition = 0;
-while (co2RatingCandidates.length > 1) {
-	co2RatingCandidates = co2RatingCandidates
-		.filter(candidate => candidate[co2RatingBitPosition] === epsilonBits.charAt(co2RatingBitPosition));
-	co2RatingBitPosition++;
-}
+	let co2ScrubberCandidates = lines.map(line => line.join(''));
+	let co2ScrubberBitIndex = 0;
+	while (co2ScrubberCandidates.length > 1) {
+		let leastCommonBit = getLeastCommonBit(co2ScrubberCandidates, co2ScrubberBitIndex);
+		if (leastCommonBit === '01') {
+			leastCommonBit = '0';
+		}
 
-const oxygenRating = parseInt(oxygenRatingCandidates[0].join(''), 2);
-const co2Rating = parseInt(co2RatingCandidates[0].join(''), 2);
+		co2ScrubberCandidates = co2ScrubberCandidates.filter(candidate => candidate.charAt(co2ScrubberBitIndex) === leastCommonBit);
+		co2ScrubberBitIndex++;
+	}
+	const [co2ScrubberRatingBitstring] = co2ScrubberCandidates;
+	const co2ScrubberRating = parseInt(co2ScrubberRatingBitstring, 2);
 
-console.log({oxygenRating, co2Rating, lifeSupportRating: oxygenRating * co2Rating});
+	console.log(oxygenGeneratorRating * co2ScrubberRating);
+})();
