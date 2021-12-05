acbf19106fe20013011008bf99d71802bfb3d089
Ben Myers <ben.myers@okstate.edu>
2021-12-04 23:39:47 -0600

    Day 5 - Part 2

--- 05/index.js
+++ 05/index.js
@@ -9,9 +9,7 @@ const lines = fs
 		const [x1, y1, x2, y2] = line.matchAll(/\d+/g);
 		return [[parseInt(x1), parseInt(y1)], [parseInt(x2), parseInt(y2)]];
 	});
-
-// Part 1
-
+	
 /**
  * 
  * @param {[[number, number], [number, number]]} line
@@ -29,16 +27,18 @@ function isHorizontal(line) {
  * @param {[[number, number], [number, number]]} line
  * @returns {boolean}
  */
- function isVertical(line) {
+	function isVertical(line) {
 	const [beginning, end] = line;
 	const [x1] = beginning;
 	const [x2] = end;
 	return x1 === x2;
 }
 
+
+// Part 1
 const nondiagonalLines = lines.filter(line => (isHorizontal(line) || isVertical(line)));
 
-/** @type {Object<String, Number>} */
+/** @type {Object<string, number>} */
 const part1Field = {};
 
 for (const line of nondiagonalLines) {
@@ -71,5 +71,61 @@ for (const line of nondiagonalLines) {
 	}
 }
 
-const numOverlaps = Object.values(part1Field).filter(count => count > 1).length;
-console.log(numOverlaps);
+const numPart1Overlaps = Object.values(part1Field).filter(count => count > 1).length;
+console.log(numPart1Overlaps);
+
+// Part 2
+/** @type {Object<string, number>} */
+const part2Field = {};
+
+for (const line of lines) {
+	const [[x1, y1], [x2, y2]] = line;
+
+	if (isHorizontal(line)) {
+		let beginning = Math.min(x1, x2);
+		let end = Math.max(x1, x2);
+		
+		for (let i = beginning; i <= end; i++) {
+			let key = `${i},${y1}`;
+			if (part2Field[key]) {
+				part2Field[key]++;
+			} else {
+				part2Field[key] = 1;
+			}
+		}
+	} else if (isVertical(line)) {
+		let beginning = Math.min(y1, y2);
+		let end = Math.max(y1, y2);
+		
+		for (let i = beginning; i <= end; i++) {
+			let key = `${x1},${i}`;
+			if (part2Field[key]) {
+				part2Field[key]++;
+			} else {
+				part2Field[key] = 1;
+			}
+		}
+	} else {
+		let diagonalLength = Math.abs(x1 - x2);
+		let xStep = (x1 < x2) ? 1 : -1;
+		let yStep = (y1 < y2) ? 1 : -1;
+
+		for (let i = 0; i <= diagonalLength; i++) {
+			let x = x1 + (i * xStep);
+			let y = y1 + (i * yStep);
+
+			let key = `${x},${y}`;
+			if (part2Field[key]) {
+				part2Field[key]++;
+			} else {
+				part2Field[key] = 1;
+			}
+		}
+	}
+}
+
+const numPart2Overlaps = Object
+	.values(part2Field)
+	.filter(count => count > 1)
+	.length;
+console.log(numPart2Overlaps);
