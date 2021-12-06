e0e63bcf75fd8787b90f96c61c482454ac004bae
Ben Myers <ben.myers@okstate.edu>
2021-12-06 01:33:23 -0600

    Day 6 - Cleanup

--- 06/index.js
+++ 06/index.js
@@ -7,72 +7,75 @@ let lanternfishInput = fs
 	.split(',')
 	.map((timer) => parseInt(timer));
 
-const NUM_DAYS = 255;
-// let part1Lanternfish = [...lanternfishInput];
+const NUM_DAYS = 256;
 
-// console.log(0, part1Lanternfish);
-// for (let i = 1; i <= NUM_DAYS; i++) {
-// 	let nextDayLanternfish = part1Lanternfish.map(timer => timer - 1);
-// 	for (let j = 0; j < part1Lanternfish.length; j++) {
-// 		if (nextDayLanternfish[j] === -1) {
-// 			nextDayLanternfish[j] = 6;
-// 			nextDayLanternfish.push(8);
-// 		}
-// 	}
-// 	console.log(i, nextDayLanternfish.length);
-// 	part1Lanternfish = nextDayLanternfish;
-// }
+(function() {
+	let part1Lanternfish = [...lanternfishInput];
+	
+	for (let i = 1; i <= NUM_DAYS; i++) {
+		let nextDayLanternfish = part1Lanternfish.map(timer => timer - 1);
+		for (let j = 0; j < part1Lanternfish.length; j++) {
+			if (nextDayLanternfish[j] === -1) {
+				nextDayLanternfish[j] = 6;
+				nextDayLanternfish.push(8);
+			}
+		}
+		part1Lanternfish = nextDayLanternfish;
+	}
+	
+	console.log(part1Lanternfish.length);
+})();
 
-// console.log(part1Lanternfish.length);
+(function () {
+	/** @type {Object<number, number>} */
+	const lanternfishDescendantMemo = {};
 
-/** @type {Object<number, number>} */
-const lanternfishDescendantMemo = {};
+	/**
+	 * Each lanternfish's lineage is a subtree of it, its children, its grandchildren and so forth.
+	 * Find out how many lanternfish are in a given fish's subtree (including itself)
+	 * @param {number} birthday 
+	 * @returns {number} count of lanternfish in subtree
+	 */
+	function getFishInSubtree(birthday) {
+		// If we already know how many fish are in this subtree, use that
+		if (lanternfishDescendantMemo[birthday]) {
+			return lanternfishDescendantMemo[birthday];
+		}
 
-/**
- * 
- * @param {number} birthday 
- * @returns {number}
- */
-function getFishInSubtree(birthday) {
-	// If we already know how many fish are in this subtree, use that
-	if (lanternfishDescendantMemo[birthday]) {
-		return lanternfishDescendantMemo[birthday];
+		const thisFish = 1;
+		const birthingDays = getAllDatesForGivingBirth(birthday);
+		const descendants = birthingDays.reduce((sum, birthday) => (sum + getFishInSubtree(birthday)), 0);
+
+		// Memoize
+		lanternfishDescendantMemo[birthday] = thisFish + descendants;
+
+		return thisFish + descendants;
 	}
 
-	const thisFish = 1;
-	const birthingDays = getAllDatesForGivingBirth(birthday);
-	const descendants = birthingDays.reduce((sum, birthday) => (sum + getFishInSubtree(birthday)), 0);
+	/**
+	 * Get a list of all days on which this lanternfish will birth another
+	 * @param {number} birthday - the day this lanternfish was born
+	 * @returns {number[]} a list of birthdays for this lanternfish's children
+	 */
+	function getAllDatesForGivingBirth(birthday) {
+		// Lanternfish was born too late to birth any fish before time is up
+		if ((birthday + 7 + 2) > NUM_DAYS) {
+			return [];
+		}
 
-	// Memoize
-	lanternfishDescendantMemo[birthday] = thisFish + descendants;
+		let datesForGivingBirth = [];
+		for (
+			let nextDateToGiveBirth = birthday + 7 + 2;
+			nextDateToGiveBirth < NUM_DAYS;
+			nextDateToGiveBirth += 7
+		) {
+			datesForGivingBirth.push(nextDateToGiveBirth);
+		}
 
-	return thisFish + descendants;
-}
-
-/**
- * Get a list of all days on which this lanternfish will birth another
- * @param {number} birthday - the day this lanternfish was born
- * @returns {number[]} a list of birthdays for this lanternfish's children
- */
-function getAllDatesForGivingBirth(birthday) {
-	// Lanternfish was born too late to birth any fish before time is up
-	if ((birthday + 7 + 2) > NUM_DAYS) {
-		return [];
+		return datesForGivingBirth;
 	}
 
-	let datesForGivingBirth = [];
-	for (
-		let nextDateToGiveBirth = birthday + 7 + 2;
-		nextDateToGiveBirth <= NUM_DAYS;
-		nextDateToGiveBirth += 7
-	) {
-		datesForGivingBirth.push(nextDateToGiveBirth);
-	}
-
-	return datesForGivingBirth;
-}
-
-const originalBirthdays = lanternfishInput.map(timer => timer - (7 + 2));
-console.log(originalBirthdays);
-const allLanternfish = originalBirthdays.reduce((sum, birthday) => sum + getFishInSubtree(birthday), 0);
-console.log(allLanternfish);
+	const originalBirthdays = lanternfishInput.map(timer => timer - (7 + 2));
+	const allLanternfish = originalBirthdays.reduce((sum, birthday) => sum + getFishInSubtree(birthday), 0);
+	console.log(allLanternfish);
+})()
