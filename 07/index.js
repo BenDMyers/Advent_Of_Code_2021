03cc0f610525b2b8280e6fa2e234018753277d61
Ben Myers <ben.myers@okstate.edu>
2021-12-06 23:11:46 -0600

    Day 7 - Part 2

--- 07/index.js
+++ 07/index.js
@@ -10,19 +10,55 @@ let crabs = fs
 let min = Math.min(...crabs);
 let max = Math.max(...crabs);
 
-let minimumFuelRequired = Number.MAX_SAFE_INTEGER;
-let optimalHorizontalPosition = -1;
+// Part 1
+(function () {
+	let minimumFuelRequired = Number.MAX_SAFE_INTEGER;
+	let optimalHorizontalPosition = -1;
 
-for (let horizontal = min; horizontal <= max; horizontal++) {
-	let fuelRequired = crabs.reduce((fuel, crabPosition) => {
-		let necessaryFuelForCrab = Math.abs(horizontal - crabPosition);
-		return fuel + necessaryFuelForCrab;
-	}, 0);
+	for (let horizontal = min; horizontal <= max; horizontal++) {
+		let fuelRequired = crabs.reduce((fuel, crabPosition) => {
+			let necessaryFuelForCrab = Math.abs(horizontal - crabPosition);
+			return fuel + necessaryFuelForCrab;
+		}, 0);
 
-	if (fuelRequired < minimumFuelRequired) {
-		minimumFuelRequired = fuelRequired;
-		optimalHorizontalPosition = horizontal;
+		if (fuelRequired < minimumFuelRequired) {
+			minimumFuelRequired = fuelRequired;
+			optimalHorizontalPosition = horizontal;
+		}
 	}
-}
 
-console.log({minimumFuelRequired, optimalHorizontalPosition});
+	console.log({minimumFuelRequired, optimalHorizontalPosition});
+})();
+
+// Part 2
+(function () {
+	/**
+	 * @param {number} distance 
+	 * @returns {number}
+	 */
+	function getFuelRequired(distance) {
+		let fuelRequired = 0;
+		for (let i = 1; i <= distance; i++) {
+			fuelRequired += i;
+		}
+		return fuelRequired;
+	}
+
+	let minimumFuelRequired = Number.MAX_SAFE_INTEGER;
+	let optimalHorizontalPosition = -1;
+
+	for (let horizontal = min; horizontal <= max; horizontal++) {
+		let fuelRequired = crabs.reduce((fuel, crabPosition) => {
+			let distance = Math.abs(horizontal - crabPosition);
+			let necessaryFuelForCrab = getFuelRequired(distance);
+			return fuel + necessaryFuelForCrab;
+		}, 0);
+
+		if (fuelRequired < minimumFuelRequired) {
+			minimumFuelRequired = fuelRequired;
+			optimalHorizontalPosition = horizontal;
+		}
+	}
+
+	console.log({minimumFuelRequired, optimalHorizontalPosition});
+})();
